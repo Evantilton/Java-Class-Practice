7f5e7bfc5ff9fcd258f644b32701f154ca95dd8a
evantilton <evantilton@gmail.com>
2020-03-12 11:29:39 -0500

    char and boolean

---
+++ CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBooleanVal = true;
+        boolean myFalseBooleanVal = false;
+
+        boolean isCustomerOverTwentyOne = true;
+        
+
+
+    }
+}
